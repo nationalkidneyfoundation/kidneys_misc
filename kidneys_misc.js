37c15a9dc7a86612823ffaa43e376f1340a9bbec
Jacob Lane <lane.jacob@gmail.com>
2017-06-08 16:51:11 -0400

    promotions

--- kidneys_misc.js
+++ kidneys_misc.js
@@ -1,8 +1,65 @@
 (function ($, Drupal) {
+  Drupal.kidneys_misc = Drupal.kidneys_misc || {};
+  Drupal.kidneys_misc.getCategories = function(stringify) {
+    stringify = stringify || false;
+    var c = $.cookie('categories') || '{}';
+    c = JSON.parse(c);
+    if (c[undefined]) {
+      delete c[undefined];
+    }
+    return stringify ? JSON.stringify(c) : c;
+  }
+  Drupal.kidneys_misc.addCategory = function(c, w) {
+    var cs = Drupal.kidneys_misc.getCategories(false);
+    if (c) { {
+      cs[c] = +cs[c] ? cs[c] + (w / cs[c]) : w || 1;
+    }}
+    $.cookie('categories', JSON.stringify(cs), { path: '/' });
+  }
+  Drupal.kidneys_misc.addCategories = function(newCs) {
+    if (typeof newCs === 'string') {
+      newCs = JSON.parse(newCs);
+    }
+    $.each(newCs, function(i,v){
+      Drupal.kidneys_misc.addCategory(i,v);
+    });
+  }
   Drupal.behaviors.nkf_misc = {
     attach: function(context, settings) {
-      jQuery("#promotion--block").load("/nkf-promotion/block/test");
-      jQuery("#promotion--button").load("/nkf-promotion/button/test");
+      if (Drupal.settings.kidneys_categories && !Drupal.settings.kidneys_categories_processed) {
+        Drupal.kidneys_misc.addCategories(Drupal.settings.kidneys_categories);
+        Drupal.settings.kidneys_categories_processed = true;
+      }
+      var query = Drupal.kidneys_misc.getCategories(true);
+      $(".promotion").each(function() {
+        $(this).once(function() {
+          var base = $(this).attr('id');
+          var element_settings = {
+            url: settings.basePath + settings.pathPrefix + 'nkf-promotion?display=' + $(this).data('promo') + '&query=' + query + '&calling_q=' + Drupal.settings.calling_q,
+            event: 'fetch',
+            progress: {
+              type: 'none'
+            }
+          };
+          Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
+          $(this).trigger('fetch');
+        });
+        $(this).once().click(function(){
+          var obj = {
+            hitType: 'event',
+            eventCategory: 'promo',
+            eventAction: $(this).data('promo') ? $(this).data('promo') : '',
+            eventLabel: $('>div',this).data('promo-name') ? $('>div',this).data('promo-name') : '',
+            eventValue: $('>div',this).data('id') ? $('>div', this).data('id') : '',
+            transport: 'beacon'
+          };
+          try {
+            ga('send', obj);
+          } catch (e) {}
+        });
+      });
     }
   }
+
+
 })(jQuery, Drupal);
---
+++ kidneys_misc_pardot.js
@@ -0,0 +1,24 @@
+(function ($, Drupal) {
+  Drupal.behaviors.nkf_misc_pardot = {
+    attach: function(context, settings) {
+      $(window).on("message", function(e) {
+        var data = e.originalEvent.data;
+        if (data.submitted) {
+          console.log(data);
+          if (data.email) {
+            var iFrame = '<iframe src="http://www3.kidney.org/l/219342/2017-06-02/q74n?email=' + data.email + '" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>'
+            $('#pardot-progressive').append(iFrame);
+            $.magnificPopup.open({
+              items: {
+                src: '#pardot-progressive',
+                type: 'inline'
+              }
+            });
+          } else {
+            //$.magnificPopup.close();
+          }
+        }
+      });
+    }
+  }
+})(jQuery, Drupal);
