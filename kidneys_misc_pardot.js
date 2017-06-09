fdb9a6b845e87e9b19047dcdc27b0b5e48ec325d
Jacob Lane <lane.jacob@gmail.com>
2017-06-09 17:38:05 -0400

    fixing pardot domain

--- kidneys_misc_pardot.js
+++ kidneys_misc_pardot.js
@@ -4,7 +4,6 @@
       $(window).on("message", function(e) {
         var data = e.originalEvent.data;
         if (data.submitted) {
-          console.log(data);
           if (data.email) {
             var iFrame = '<iframe src="https://go.pardot.com/l/219342/2017-06-02/q74n?email=' + data.email + '" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>'
             $('#pardot-progressive').append(iFrame);
