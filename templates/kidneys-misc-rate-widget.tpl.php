0680e230ce4b6bac952c4bedf1685e288cfcd81c
Jacob Lane <lane.jacob@gmail.com>
2019-05-24 15:39:29 -0400

    updated rate tracking code to avoid null values being based to template file

--- templates/kidneys-misc-rate-widget.tpl.php
+++ templates/kidneys-misc-rate-widget.tpl.php
@@ -2,31 +2,31 @@
 
 
 ?>
-<div id="<?php print $id; ?>" class="rate-container">
-  <div class="question">
-    <span class="bold"><?php print $question; ?> </span>
-    <a class="js--return-false"
+<div id="rating-<?php print $id; ?>" class="rate-container">
+  <div class="question bg--gray-1 padding-x--xxs display--inline-block">
+    <span class=""><?php print $question; ?> </span>
+    <a class="js--return-false padding-left--xxs"
        data-track="event"
        data-category="<?php print $category; ?>"
        data-action="<?php print $action; ?>"
        data-label="yes"
        data-value="<?php print $value; ?>"
        data-toggle="class"
-       data-target="<?php print $id; ?> > div"
+       data-target="#rating-<?php print $id; ?> > div"
        data-class="hide"
        href="#"><?php print $yes_label; ?></a>
-    <a class="js--return-false"
+    <a class="js--return-false padding-left--xxs"
        data-track="event"
        data-category="<?php print $category; ?>"
        data-action="<?php print $action; ?>"
        data-label="no"
        data-value="<?php print $value; ?>"
        data-toggle="class"
-       data-target="<?php print $id; ?> > div"
+       data-target="#rating-<?php print $id; ?> > div"
        data-class="hide"
        href="#"><?php print $no_label; ?></a>
    </div>
-   <div class="thanks">
+   <div class="hide thanks bg--gray-1 padding-x--xxs display--inline-block">
      <?php print $thanks;?>
    </div>
 </div>
